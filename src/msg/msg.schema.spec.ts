68f8a7ce99562a981f93457817b0cb9836b17faf
fbaptista <chico.baptista@gmail.com>
2022-05-08 17:40:55 -0300

    test(msg-schema): add test for missing property

--- src/msg/msg.schema.spec.ts
+++ src/msg/msg.schema.spec.ts
@@ -8,24 +8,42 @@ chai.use(chaiAsPromised)
 const { expect } = chai
 
 describe('MsgRequest Schema', () => {
-    it('should validate schema of an object', async () => {
+    const validateOptions = {
+        stripUnknown: true,
+    }
+    describe('On a valid request', () => {
         const mockMsg = {
             msg: 'testMsg',
             msg1: 'testErrorMsg',
         }
-        const validatedData = await msgRequestSchema.validate(mockMsg, {
-            stripUnknown: true,
-        })
+        it('should validate schema and strip unkown properties of an object', async () => {
+            const validatedData = await msgRequestSchema.validate(
+                mockMsg,
+                validateOptions,
+            )
 
-        expect(validatedData).to.have.property(
-            'msg',
-            mockMsg.msg,
-            'should have msg property with value intact',
-        )
-        expect(validatedData).to.not.have.property(
-            'msg1',
-            mockMsg.msg1,
-            'should strip unknown property',
-        )
+            expect(validatedData).to.have.property(
+                'msg',
+                mockMsg.msg,
+                'should have msg property with value intact',
+            )
+            expect(validatedData).to.not.have.property(
+                'msg1',
+                mockMsg.msg1,
+                'should strip unknown property',
+            )
+        })
+    })
+
+    describe('On an invalid request', () => {
+        it('should throw if no msg field is present', async () => {
+            const res = msgRequestSchema.validate(
+                { msg1: 'test' },
+                validateOptions,
+            )
+            await expect(res).to.eventually.be.rejectedWith(
+                'msg is a required field',
+            )
+        })
     })
 })
