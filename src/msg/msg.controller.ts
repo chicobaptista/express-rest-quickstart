edeab1a12162d72a8cd1aae69678e820ed4e20d4
fbaptista <chico.baptista@gmail.com>
2022-05-08 22:31:11 -0300

    feat: createMsg controller

--- src/msg/msg.controller.ts
+++ src/msg/msg.controller.ts
@@ -1,14 +1,11 @@
-import { MsgRequest, msgRequestSchema } from './msg.schema'
 import { NextFunction, Request, Response } from 'express'
 import { MessageController } from './msg.controller.interface'
+import { MsgRequest } from './msg.schema'
 
 async function createMessage(req: Request, res: Response, next: NextFunction) {
     try {
-        const { body } = req
-        const data: MsgRequest = await msgRequestSchema.validate(body, {
-            stripUnknown: true,
-        })
-        return res.status(201).json(data)
+        const { body }: { body: MsgRequest } = req
+        return res.status(201).json(body)
     } catch (error) {
         next(error)
     }
