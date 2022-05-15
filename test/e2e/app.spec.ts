239997ea9f16c02806ad153db595c0911c113347
fbaptista <chico.baptista@gmail.com>
2022-05-15 16:12:20 -0300

    test: describe msg route test

--- test/e2e/app.spec.ts
+++ test/e2e/app.spec.ts
@@ -5,10 +5,12 @@ import chaiHttp = require('chai-http')
 
 chai.use(chaiHttp)
 
-describe('Root route', () => {
-    it(`should return the msg param`, async () => {
-        const testBody = { msg: 'testMsg' }
-        const res = await chai.request(app).post('/msg').send(testBody)
-        expect(res.body.msg).to.include(testBody.msg)
+describe('Message route', () => {
+    describe('POST method', () => {
+        it(`should return the msg param`, async () => {
+            const testBody = { msg: 'testMsg' }
+            const res = await chai.request(app).post('/msg').send(testBody)
+            expect(res.body.msg).to.include(testBody.msg)
+        })
     })
 })
