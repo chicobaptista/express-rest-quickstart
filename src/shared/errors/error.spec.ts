c34cada146d99b1dea87c960d03c6217f13ba80d
fbaptista <chico.baptista@gmail.com>
2022-05-15 21:24:23 -0300

    refactor(error-test): refactor test to use default object expectations

--- src/shared/errors/error.spec.ts
+++ src/shared/errors/error.spec.ts
@@ -3,20 +3,25 @@ import { ApiError } from './error'
 import { expect } from 'chai'
 
 describe('ApiError', () => {
+    const DEFAULTS = {
+        name: 'ApiError',
+        status: 500,
+        message: 'Something went wrong. Please try again.',
+    }
     it('should have default Internal Server Error atributes', () => {
         try {
             throw new ApiError()
         } catch (error) {
             expect(error.name).to.equal(
-                'ApiError',
+                DEFAULTS.name,
                 'should have the default ApiError name',
             )
             expect(error.status).to.equal(
-                500,
+                DEFAULTS.status,
                 'should have internal server error status as default',
             )
             expect(error.message).to.equal(
-                'Something went wrong. Please try again.',
+                DEFAULTS.message,
                 'should have generic error message by default',
             )
             expect(!!error.stack, 'should capture the call stack trace').to.be
@@ -25,19 +30,21 @@ describe('ApiError', () => {
     })
 
     it('should have the attributes passed by the constructor', () => {
+        const customStatus = 409
+        const customMessage = 'Custom error message'
         try {
-            throw new ApiError('Custom error message', 409)
+            throw new ApiError(customMessage, customStatus)
         } catch (error) {
             expect(error.name).to.equal(
-                'ApiError',
+                DEFAULTS.name,
                 'should have the default ApiError name',
             )
             expect(error.status).to.equal(
-                409,
+                customStatus,
                 'should have status passed on constructor',
             )
             expect(error.message).to.equal(
-                'Custom error message',
+                customMessage,
                 'should have message passed on constructor',
             )
             expect(!!error.stack, 'should capture the call stack trace').to.be
@@ -45,19 +52,20 @@ describe('ApiError', () => {
         }
     })
     it('should handle optional parameters passed on constructor', () => {
+        const customStatus = 403
         try {
-            throw new ApiError(undefined, 403)
+            throw new ApiError(undefined, customStatus)
         } catch (error) {
             expect(error.name).to.equal(
-                'ApiError',
+                DEFAULTS.name,
                 'should have the default ApiError name',
             )
             expect(error.status).to.equal(
-                403,
+                customStatus,
                 'should status passed on constructor',
             )
             expect(error.message).to.equal(
-                'Something went wrong. Please try again.',
+                DEFAULTS.message,
                 'should have generic error message by default',
             )
             expect(!!error.stack, 'should capture the call stack trace').to.be
