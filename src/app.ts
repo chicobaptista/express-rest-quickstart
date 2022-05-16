6dfd1c140472d976870446f22850816a5c56fba9
fbaptista <chico.baptista@gmail.com>
2022-05-15 23:27:48 -0300

    feat(error): create simple error logger

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 /* istanbul ignore file */
+import { errorLoggerMiddleware } from './shared/errors/errorLogger.middleware'
 import { errorParserMiddleware } from './shared/errors/errorParser.middleware'
 import express from 'express'
 import router from './routes/routes'
@@ -8,6 +9,7 @@ const port = 3000
 
 app.use(express.json())
 app.use(router)
+app.use(errorLoggerMiddleware)
 app.use(errorParserMiddleware)
 app.listen(port, () => {
     console.log(`Application is running on port ${port}.`)
